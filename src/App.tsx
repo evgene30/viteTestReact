80b628d09c97a35385ea1f8fe1c798755edb771c
Akulich Evgeniy <evgene.fe@gmail.com>
2022-12-29 22:49:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import './App.scss';
+import { Input } from './components/Input';
+
+const App: FC = () => (
+  <div className="App">
+    <Input />
+  </div>
+);
+export default App;
