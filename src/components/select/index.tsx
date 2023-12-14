e6339c31d6ddc9c00c4659ea91f1b1c009379f6c
Akulich Evgeniy <evgene.fe@gmail.com>
2023-12-15 00:07:19 +0300

    add autocompleat

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-void */
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
+  Autocomplete,
   Box,
+  Button,
+  CircularProgress,
   FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
   Switch,
+  TextField,
   Typography,
 } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
@@ -18,7 +21,7 @@ import {
 
 export type Option = {
   id: string;
-  value?: string;
+  title: string;
   label: string;
 };
 
@@ -26,17 +29,23 @@ type SelectLabelsProps = {
   setData: (value: { Option: string; switch: boolean }) => void;
 };
 
-const options: Option[] = [
-  { id: '1', value: 'option1', label: 'Option 1' },
-  { id: '2', value: 'option2', label: 'Option 2' },
-  { id: '3', value: 'option3', label: 'Option 3' },
+const potions: Option[] = [
+  { id: '1', title: 'option1', label: 'Option 1' },
+  { id: '2', title: 'option2', label: 'Option 2' },
+  { id: '3', title: 'option3', label: 'Option 3' },
 ];
 
 type TOptionState = { selectedOption: Array<Option>; switch: boolean };
 export const SelectLabels = ({ setData }: SelectLabelsProps) => {
+  const [checked, setChecked] = useState(false);
+  const [options, setOptions] = useState<Option[]>(potions);
+  const [open, setOpen] = React.useState(false);
+  const loading = open && checked && options.length === 0;
+
   const {
     control,
     watch,
+    resetField,
     formState: { errors },
   } = useForm<TOptionState>({
     mode: 'onChange',
@@ -47,50 +56,99 @@ export const SelectLabels = ({ setData }: SelectLabelsProps) => {
   });
 
   useEffect(() => {
-    const subscription = watch((value) => setData(value as never));
+    const subscription = watch((value) => {
+      setData(value as never);
+    });
 
     return () => {
       subscription.unsubscribe();
     };
   }, [setData, watch]);
 
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    void (async () => {
+      const { data } = await axios.get('https://fakestoreapi.com/products');
+      if (active) {
+        setOptions([...data]);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  useEffect(() => {
+    if (!open && checked) {
+      setOptions([]);
+    }
+    if (!checked) {
+      setOptions(potions);
+    }
+  }, [checked, open]);
+
   return (
     <form>
       <Box sx={formStyles}>
         <Box sx={selectStyles}>
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Options</InputLabel>
             <Controller
               name="selectedOption"
               control={control}
-              render={({ field: { onChange, value, ...field } }) => (
-                <Select
+              render={({ field: { onChange, value, name, ...field } }) => (
+                <Autocomplete
                   {...field}
-                  multiple
-                  value={value || []}
-                  onChange={(e) => {
-                    const selectedOptions = e.target.value;
-                    onChange(selectedOptions);
+                  open={open}
+                  onOpen={() => {
+                    setOpen(true);
                   }}
-                  label="Options"
-                  renderValue={(selectedIds) => {
-                    const selectedValues = selectedIds.map((id) => {
-                      const option = options.find(
-                        (o: Option) => o.id === (id as never),
-                      );
-
-                      return option ? option.label : '';
-                    });
-
-                    return selectedValues.join(', ');
+                  onClose={() => {
+                    setOpen(false);
                   }}
-                >
-                  {options.map((option) => (
-                    <MenuItem key={option.id} value={option.id}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Select>
+                  loading={loading}
+                  fullWidth
+                  isOptionEqualToValue={(option, dataVal) =>
+                    option.id === dataVal.id
+                  }
+                  clearIcon={false}
+                  // isOptionEqualToValue={(option, dataVal) => {
+                  //   if (option.title === dataVal.title) {
+                  //     reset();
+                  //   }
+
+                  //   return option.title === dataVal.title;
+                  // }}
+                  inputValue={value.title || ''}
+                  options={options}
+                  clearOnEscape
+                  getOptionLabel={(option) => option.title}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label={name}
+                      InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                          <>
+                            {loading ? (
+                              <CircularProgress color="inherit" size={20} />
+                            ) : null}
+                            {params.InputProps.endAdornment}
+                          </>
+                        ),
+                      }}
+                    />
+                  )}
+                  onChange={(e, data) => {
+                    onChange(data);
+                  }}
+                />
               )}
             />
             {errors.selectedOption && (
@@ -106,13 +164,23 @@ export const SelectLabels = ({ setData }: SelectLabelsProps) => {
               render={({ field: { value, ...field } }) => (
                 <Switch
                   {...field}
-                  checked={value}
+                  value={value}
                   onChange={(e) => field.onChange(e.target.checked)}
                 />
               )}
             />
             <Typography fontSize={14}>Check option</Typography>
           </Box>
+          <Button
+            onClick={() => {
+              setChecked(!checked);
+              resetField('selectedOption', { defaultValue: [] });
+            }}
+            variant="outlined"
+            color={checked ? 'error' : 'success'}
+          >
+            Load data
+          </Button>
         </Box>
       </Box>
     </form>
