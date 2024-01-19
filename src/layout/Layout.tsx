2f212c4c3cdb9fd43a146992aac5171f069824ea
Evgeniy Akulich <evgene.fe@gmail.com>
2024-01-19 11:22:44 +0300

    fix

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, SxProps, Theme } from '@mui/material';
 import { layoutStyle } from '@/layout/styles';
 import { Header } from '@/components/header';
 import { Home } from '@/pages/home/Home';
@@ -10,7 +10,7 @@ export const Layout: FC = () => {
   const location = useLocation();
 
   return (
-    <Box component="main" sx={layoutStyle}>
+    <Box component="main" sx={layoutStyle as SxProps<Theme>}>
       <Header />
       <Box className="content">
         {location.pathname === '/' ? <Home /> : <Outlet />}
