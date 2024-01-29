59579355d828f5829c78d1eec28aa7338100f0df
Evgeniy Akulich <evgene.fe@gmail.com>
2024-01-29 11:27:04 +0300

    fix

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+  Box,
+  Button,
+  TextField,
+  Autocomplete,
+  AutocompleteProps,
+} from '@mui/material';
 import { aboutStyle } from './style';
-import { SortableTable } from '@/components/sortableTable/SortTable';
 
-export const About = () => (
-  <Box sx={aboutStyle}>
-    <SortableTable />
-  </Box>
-);
+export const About = () => {
+  function createMuiComponent<T>(
+    Component: React.ComponentType<T>,
+    defaultProps: T,
+  ) {
+    return function MuiComponent(props: T & React.Attributes) {
+      return <Component {...defaultProps} {...props} />;
+    };
+  }
+
+  const PrimaryButton = createMuiComponent(Button, {
+    variant: 'contained',
+    color: 'primary',
+  });
+  const SecondaryButton = createMuiComponent(Button, {
+    variant: 'outlined',
+    color: 'secondary',
+  });
+  const DefaultTextField = createMuiComponent(TextField, {
+    variant: 'outlined',
+  });
+
+  const AutocompleteComponent = createMuiComponent<
+    AutocompleteProps<unknown, boolean, boolean, boolean>
+  >(Autocomplete, {
+    renderInput: (params) => (
+      <TextField {...params} label="Выберите вариант" variant="outlined" />
+    ),
+    options: ['1', '3'],
+  });
+
+  return (
+    <Box sx={aboutStyle as never}>
+      <AutocompleteComponent />
+    </Box>
+  );
+};
