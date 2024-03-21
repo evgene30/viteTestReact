2cad8f3f0ee25823b425a2c0efe822ccfb6cbf4e
Evgeniy Akulich <evgene.fe@gmail.com>
2024-03-21 12:22:53 +0300

    was del single-spa

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'single-spa-entry.ts',
-      name: 'YourAppName',
-      fileName: (format) => `your-app-name.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['single-spa'],
-      output: {
-        globals: {
-          'single-spa': 'SingleSPA',
-        },
-      },
-    },
-  },
   plugins: [
     react(),
     VitePWA({
