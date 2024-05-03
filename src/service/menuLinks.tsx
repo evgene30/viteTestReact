e54d03a66dbe1682505117652d2d3ccb81aa2ef3
Evgeniy Akulich <evgene.fe@gmail.com>
2024-05-03 14:10:34 +0300

    was fixed routing

--- src/service/menuLinks.tsx
+++ src/service/menuLinks.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { About } from '@/pages/about/About';
+import { Home } from '@/pages/home/Home';
 
-export type TRoutes = { [key: string]: string };
-
-export const routes: TRoutes = {
-  Home: '/',
-  About: '/about',
-  Work: '/work',
-};
-
-const getComponent = (key: string) => {
-  switch (key) {
-    case 'About':
-      return <About />;
-    case 'Work':
-      return <div>W1</div>;
-    default:
-      return <div>{key}</div>;
-  }
-};
-
-export const menuLinks = Object.keys(routes).map((key) => ({
-  title: key,
-  link: routes[key],
-  path: key.toLocaleLowerCase(),
-  element: getComponent(key),
-}));
+export const menuLinks = [
+  {
+    link: '/',
+    title: 'Home',
+    path: '/',
+    element: <Home />,
+  },
+  {
+    link: '/about',
+    title: 'About',
+    path: '/about',
+    element: <About />,
+  },
+  {
+    link: '/work',
+    title: 'Work',
+    path: '/work',
+    element: <div>W1</div>,
+  },
+];
