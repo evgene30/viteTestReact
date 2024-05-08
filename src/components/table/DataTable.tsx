76d2c7aaf9287bafa6e894028512a46118fb793f
Evgeniy Akulich <evgene.fe@gmail.com>
2024-05-08 16:06:15 +0300

    fix table

--- src/components/table/DataTable.tsx
+++ src/components/table/DataTable.tsx
@@ -1,41 +1,39 @@
 import React, { memo, useState } from 'react';
 import { Box, LinearProgress } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, useGridApiRef } from '@mui/x-data-grid';
 import { useLoaderData } from 'react-router-dom';
+
 import { noResultStyles } from '@/components/table/styles';
-import { PaginationComponent } from '@/components/pagination/Pagination';
 import { LoadUsersType } from '@/routes/loaders/getLoadUsers';
 import { columnsConfig } from './tableConfig';
 
 export const DataTable = memo((): JSX.Element => {
   const { users } = useLoaderData() as LoadUsersType;
-  const [page, setPage] = useState(1);
+
+  const apiRef = useGridApiRef();
+
   const [paginationModel, setPaginationModel] = useState({
-    pageSize: 10,
-    page,
+    pageSize: 5,
+    page: 1,
   });
 
   return (
     <Box sx={{ width: '100%' }}>
       <DataGrid
         slots={{
-          pagination: () => (
-            <PaginationComponent
-              page={page}
-              setPage={setPage}
-              count={Number(users.length / paginationModel.pageSize) || 1}
-            />
-          ),
           loadingOverlay: LinearProgress,
           noRowsOverlay: () => <Box sx={noResultStyles}>No result</Box>,
         }}
+        rowCount={users.length}
         loading={!users}
         rows={users}
         autoHeight
+        pageSizeOptions={[5, 10, 25]}
         columns={columnsConfig}
         paginationModel={paginationModel}
         onPaginationModelChange={setPaginationModel}
         disableRowSelectionOnClick
+        apiRef={apiRef}
       />
     </Box>
   );
