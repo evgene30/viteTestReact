84c55469d0bc1ec9d7a95b2b591677c8a768f611
EVGENE AKULICH <evgene.fe@gmail.com>
2024-07-30 23:25:22 +0300

    was fixed tests

--- src/__tests__/Dialog.test.tsx
+++ src/__tests__/Dialog.test.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { DialogSelect } from '@/components/dialog/Dialog';
+import { ModalProvider } from '@/components/modal/ModalContext';
+import { Modal } from '@/components/modal/Modal';
+
+const component = () => (
+  <ModalProvider>
+    <DialogSelect />
+    <Modal />
+  </ModalProvider>
+);
 
 describe('DialogSelect', () => {
   test('renders the open modal button', () => {
-    render(<DialogSelect />);
+    render(component());
     const openModalButton = screen.getByRole('button', { name: /open modal/i });
     expect(openModalButton).toBeInTheDocument();
   });
 
   test('opens the dialog when the open modal button is clicked', () => {
-    render(<DialogSelect />);
+    render(component());
     const openModalButton = screen.getByRole('button', { name: /open modal/i });
     fireEvent.click(openModalButton);
-    const dialogTitle = screen.getByText(/tested form/i);
+    const dialogTitle = screen.getByText(/open modal title/i);
     expect(dialogTitle).toBeInTheDocument();
   });
 
   test('closes the dialog when the cancel button is clicked', () => {
-    render(<DialogSelect />);
+    render(component());
     const openModalButton = screen.getByRole('button', { name: /open modal/i });
     fireEvent.click(openModalButton);
     const cancelButton = screen.getByRole('button', { name: /cancel/i });
