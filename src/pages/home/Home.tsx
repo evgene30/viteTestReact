8ab263f4a22bee33c21f5919ed216236d9e8bf51
EVGENE AKULICH <evgene.fe@gmail.com>
2024-09-09 23:00:33 +0300

    add input

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,3 +9,34 @@ export const Home = () => (
     <DataTable />
   </Box>
 );
+
+// import React, { useState } from 'react';
+// import './index.css';
+// import { Input } from 'antd';
+
+// const options = [
+//   { id: 23, value: 'Text' },
+//   { id: 42, value: 'Другой текст' },
+// ];
+
+// const App: React.FC = () => {
+//   const [selectedId, setSelectedId] = useState(42);
+
+//   const handleChange = (event) => {
+//     const selectedOption = options.find(
+//       (option) => option.value === event.target.value
+//     );
+//     setSelectedId(selectedOption?.id ?? null);
+//   };
+
+//   return (
+//     <Input
+//       type="text"
+//       variant='filled'
+//       value={options.find((option) => option.id === selectedId)?.value ?? ''}
+//       onChange={handleChange}
+//     />
+//   );
+// };
+
+// export default App;
