b6242b58940ff7ec70f4ccbe9d650e9c66148365
EVGENE AKULICH <evgene.fe@gmail.com>
2024-10-26 19:44:15 +0300

    was added panels

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,10 +2,12 @@ import { Box } from '@mui/material';
 import { paginationStyles } from '@/pages/home/style';
 import { DataTable } from '@/components/table/DataTable';
 import { DialogSelect } from '@/components/dialog/Dialog';
+import { SplitPanels } from '@/components/splitPanels/SplitPanels';
 
 export const Home = () => (
   <Box sx={paginationStyles}>
     <DialogSelect />
     <DataTable />
+    <SplitPanels />
   </Box>
 );
