ad625519aee732ea3a2f6b71e937163155ed8550
EVGENE AKULICH <evgene.fe@gmail.com>
2025-01-21 00:15:12 +0300

    was install antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { TableComponent, Option } from './components/Table';
 
-const App: FC = () => <div>Test</div>;
+const App: FC = () => {
+  const [options, setOptions] = useState<Option[]>([
+    { label: 'Secret', field: '2323', link: '12122' },
+  ]);
+
+  return (
+    <div style={{ width: '1160px', margin: '0 auto' }}>
+      <TableComponent options={options} setOptions={setOptions} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Table, Button, Tooltip, Select, Input } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+
+export interface Option {
+  label: string;
+  field: string;
+  link: string;
+}
+
+interface MyTableComponentProps {
+  options: Option[];
+  setOptions: React.Dispatch<React.SetStateAction<Option[]>>;
+}
+
+export const TableComponent: React.FC<MyTableComponentProps> = ({
+  options,
+  setOptions,
+}) => {
+  const handleDelete = (record: Option) => {
+    setOptions(options.filter((item) => item.field !== record.field));
+  };
+
+  const handleChange = (value: unknown, record: Option) => {
+    setOptions((prev: Option[]) =>
+      prev.map((item) => {
+        if (item.label === record.label) {
+          return { ...item, link: value as string };
+        }
+
+        return item;
+      }),
+    );
+  };
+
+  const handleInputChange = (value: unknown, record: Option) => {
+    setOptions((prev: Option[]) =>
+      prev.map((item) => {
+        if (item.label === record.label) {
+          return { ...item, field: value as string };
+        }
+
+        return item;
+      }),
+    );
+  };
+
+  console.log(options);
+
+  const columns = [
+    {
+      title: 'Индекс',
+      dataIndex: 'Индекс',
+      key: 'Индекс',
+      render: (text: string, record: Option, index: number) => `${index + 1}`,
+      width: '5%',
+    },
+    {
+      title: 'Label',
+      dataIndex: 'label',
+      key: 'label',
+      width: '30%',
+    },
+    {
+      title: 'Input',
+      dataIndex: 'Input',
+      key: 'Input',
+      width: '30%',
+      render: (text: string, record: Option) => (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Input
+            value={record.field}
+            onChange={(e) => handleInputChange(e.target.value, record)}
+          />
+        </div>
+      ),
+    },
+    {
+      title: 'Select',
+      dataIndex: 'Select',
+      key: 'Select',
+      width: '30%',
+      render: (text: string, record: Option) => (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Select
+            value={record.link}
+            placeholder="Выберите параметр"
+            options={[
+              { value: 'jack', label: 'Jack' },
+              { value: 'lucy', label: 'Lucy' },
+              { value: 'Yimi', label: 'yimi' },
+            ]}
+            style={{ width: '100%' }}
+            onChange={(value) => handleChange(value, record)}
+          />
+        </div>
+      ),
+    },
+    {
+      title: 'Действие',
+      key: 'action',
+      width: '5%',
+      render: (text: string, record: Option) => (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Tooltip title="Удалить">
+            <Button
+              size="large"
+              shape="circle"
+              type="text"
+              icon={<DeleteOutlined />}
+              onClick={() => handleDelete(record)}
+            />
+          </Tooltip>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      rowHoverable={false}
+      dataSource={options}
+      columns={columns}
+      rowKey="field"
+      bordered
+      pagination={false}
+      size="small"
+    />
+  );
+};
